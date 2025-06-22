945fe3d080cb344723bbdc3685ddb05ca5851461
Alexander Nikulin <snakky@gmail.com>
2025-06-22 23:18:18 +0400

    Add keymap to change current working directory to one where a current open file is.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -64,3 +64,7 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+vim.keymap.set("n", "<leader>cd", function()
+  vim.cmd("cd " .. vim.fn.expand("%:p:h"))
+end, { desc = "CD to current file's directory" })
