1d838611dd5294b6dc2f49d92c9576c4032b3c5b
Alexander Nikulin <snakky@gmail.com>
2025-06-23 01:07:08 +0400

    Make cmp plugin accept selected item on enter.

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -83,9 +83,9 @@ return
 				-- No, but seriously. Please read `:help ins-completion`, it is really good!
 				mapping = cmp.mapping.preset.insert {
 					-- Select the [n]ext item
-					['<C-n>'] = cmp.mapping.select_next_item(),
+					--['<C-n>'] = cmp.mapping.select_next_item(),
 					-- Select the [p]revious item
-					['<C-p>'] = cmp.mapping.select_prev_item(),
+					--['<C-p>'] = cmp.mapping.select_prev_item(),
 
 					-- Scroll the documentation window [b]ack / [f]orward
 					['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -98,9 +98,9 @@ return
 
 					-- If you prefer more traditional completion keymaps,
 					-- you can uncomment the following lines
-					--['<CR>'] = cmp.mapping.confirm { select = true },
-					--['<Tab>'] = cmp.mapping.select_next_item(),
-					--['<S-Tab>'] = cmp.mapping.select_prev_item(),
+					['<CR>'] = cmp.mapping.confirm { select = true },
+					['<Tab>'] = cmp.mapping.select_next_item(),
+					['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
 					-- Manually trigger a completion from nvim-cmp.
 					--  Generally you don't need this, because nvim-cmp will display
